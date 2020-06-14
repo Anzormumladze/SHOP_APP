0db25382fb12debf9fbfdd4fb3f44642a8ce8f52
Anzormumladze <anzor.anzor.mumladze@gmail.com>
2020-06-14 18:02:48 +0400

    added screens for user and shop

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ProductsOverviewScreen = () => {
+  return (
+    <View>
+      <Text>ProductsOverviewScreen</Text>
+    </View>
+  );
+};
+
+export default ProductsOverviewScreen;
---
+++ screens/user/userProductsScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const userProductsScreen = () => {
+  return (
+    <View>
+      <Text>userProductsScreen</Text>
+    </View>
+  );
+};
+
+export default userProductsScreen;
